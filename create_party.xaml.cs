e1fb8d5dd73942b099b7a0ad390e395cc6ac9afa
dujin kim <kdj00718@gmail.com>
2021-12-18 21:45:06 -0800

    working on create a party

---
+++ create_party.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for create_party.xaml
+    /// </summary>
+    public partial class create_party : Window
+    {
+
+        List<string> names;
+
+        public create_party()
+        {
+            InitializeComponent();
+            names = new List<string>();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            names.Add(playerName.Text);
+            playerName.Text = "";
+            nameList.ItemsSource = new List<Object>();
+            nameList.ItemsSource = names;
+            //displayPlayers();
+        }
+
+        private void start_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void displayPlayers()
+        {
+
+            foreach (string name in names) {
+                System.Diagnostics.Trace.WriteLine(name);
+            }
+        }
+
+       
+    }
+}
